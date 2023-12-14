29a28b6cb547c5b43569d6fe54d6bc1200e60d32
Eagleast <113238235+Eagleast@users.noreply.github.com>
2023-12-14 20:13:28 +0100

    added things xd

--- Scenes/jumper.gd
+++ Scenes/jumper.gd
@@ -2,45 +2,41 @@ extends CharacterBody2D
 
 var concected_platforms = []
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -700.0
 @onready var area_2d = $Area2D
 var mass_player = 100
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+#func _ready():
+	#set_floor_snap_length(10)
+	#apply_floor_snap()
+	#print(get_floor_snap_length())
 
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-		move_and_slide()
+	
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept"):
 		jump()
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
 
 
 
 func jump():
-	velocity.y = JUMP_VELOCITY
-	move_and_slide()
+	var screen_size = get_window().size
+	var mouse_pos = get_viewport().get_mouse_position()
+	var jump_direction = mouse_pos - Vector2(screen_size /2)
+	var true_jump_dir = - jump_direction.normalized()
 	var connected_bodys = area_2d.get_overlapping_bodies()
-	print("conected ", connected_bodys)
+	
+	velocity = true_jump_dir * JUMP_VELOCITY
 	for j in range(len(connected_bodys)):
-		print(connected_bodys[j])
 		if connected_bodys[j].is_in_group("platform"):
-			print("platform ", connected_bodys[j], " added")
 			concected_platforms.append(connected_bodys[j])
 	for i in range(len(concected_platforms)):
-		print("impulsed")
 		concected_platforms[i].impulse(position, JUMP_VELOCITY, mass_player)
-		print("mass plaer ", mass_player)
--- Scenes/platform_movable.gd
+++ Scenes/platform_movable.gd
@@ -3,6 +3,4 @@ var mass_platform = 300
 var scaling = 0.3
 
 func impulse(mass_pos, jump_dir,mass_player):
-	print("mass player inside ", mass_player)
-	apply_impulse((Vector2(0, jump_dir*-scaling)), Vector2(0,0))
-	print("scale ", scaling)
+	apply_impulse((Vector2(0, jump_dir*-scaling)),mass_pos-position)
