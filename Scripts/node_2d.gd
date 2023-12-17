378957de4a65e1c79332e8764fedba62add04603
Eagleast <113238235+Eagleast@users.noreply.github.com>
2023-12-17 11:29:28 +0100

    signals changed
    les signaux sont mainenant ecrits dans le code

--- Scripts/node_2d.gd
+++ Scripts/node_2d.gd
@@ -1,4 +1,7 @@
 extends Node2D
+func _ready():
+	var platformplacer = get_node("platform_placer")
+	platformplacer.place_platform.connect(_on_platform_placer_place_platform)
 
 func _on_platform_placer_place_platform(platform_type, Position):
 	var spawn_platform = platform_type.instantiate()
